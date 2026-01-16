a7efd6cc7f0dfc852fd2b4762cc685fc549c677f
KrishnaVaghasia <vaghasiakrishna11@gmail.com>
2026-01-15 20:59:10 -0500

    sample ngmodule

---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+// import { NgModule } from "@angular/core";
+// import { BrowserModule } from "@angular/platform-browser";
+// import { App } from "./app";
+// import { Tasks } from "./tasks/tasks";
+// import { User } from "./user/user";
+// import { Header } from "./header/header";
+
+// @NgModule({
+//     declarations: [App], //need to make App component non standalone //non standalone components
+//     bootstrap: [App], //angular will bootstrap app from this module/component mentioned here
+//     imports: [BrowserModule,Header, Tasks, User] //standalone component used in app component and also BrowserComponent as its required
+// })
+// export class AppModule {
+
+// }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+// import { AppModule } from './app/app.module';
+// import {platformBrowserDynamic} from '@angular/platfrom-browser-dynamic'; //for module based components not standalone
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
+
+// platformBrowserDynamic().bootstrapModule(AppModule);
